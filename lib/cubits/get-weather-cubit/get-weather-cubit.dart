d758de6f9f515a8c1ee2dc495bda75b8d6f9549d
Andrew Safwat <andrew.realme90@gmail.com>
2025-03-29 12:54:28 +0200

    at first after 25

---
+++ lib/cubits/get-weather-cubit/get-weather-cubit.dart
@@ -0,0 +1,8 @@
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:gitweather3/cubits/get-weather-cubit/get-weather-states.dart';
+// import 'package:gitweather3/models/weather-model.dart';
+
+// class GetWeatherCubit extends Cubit<WatherStates> {
+// // WeatherModel weatherModel = 
+
+// // }
---
+++ lib/cubits/get-weather-cubit/get-weather-states.dart
@@ -0,0 +1,7 @@
+class WatherStates {}
+
+class WeatherLoadedState extends WatherStates {}
+
+class NoWeathereState extends WatherStates {}
+
+class WeatherFailureState extends WatherStates {}
